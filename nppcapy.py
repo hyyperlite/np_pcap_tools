0163a48abedb99ce6b383ca209cae2bd409661bd
npetersen <77npete@gmail.com>
2023-10-03 13:34:29 -0400

    * missing_packet_to_pcap now correctly outputs only the missing packets to pcap * moved some code for processing input pcaps to the npcapy module and out of script * pcap input processing now filters the max files to process at read time rather   then needing to track max files during packet comparsion checking.  simplifies code overall.

--- nppcapy.py
+++ nppcapy.py
@@ -3,6 +3,33 @@
  Module to support pcap analysis using scapy's rdpcap and associated object classes.
 """
 from scapy.all import *
+import os
+import pathlib
+
+def get_pcap_files(pcap_dir:str, max_files:int = 0):
+    if os.path.exists(pcap_dir):
+        try:
+            pcap_files = os.listdir(pcap_dir)
+        except Exception as e:
+            return e
+
+        # check extension for some limited attempt to verify files are pcaps
+        if len(pcap_files) > 0:
+            for f in pcap_files:
+                if pathlib.Path(f).suffix not in ['.pcap']:
+                    pcap_files.remove(f)
+
+        if len(pcap_files) > 0:
+            # return at most the max requested number of pcap files
+            if max_files == 0:
+                return pcap_files
+            else:
+                # list slicing to only return list with files up to max files
+                return pcap_files[0:max_files]
+        else:
+            return False
+    else:
+        return False
 
 
 def check_tcp_flags(pkt):
@@ -24,51 +51,6 @@ def check_tcp_flags(pkt):
         case 0x80:
             return 'CWR'
 
-def check_packet_attributes(pkt: scapy.layers.l2.Ether, pkt_label='pkt', debug=False):
-    """
-    :param pkt: scapy packet from rdpcap read of pcap file
-    :param pkt_label: label for packet when details output
-    :return: True if packet meets criteria, False otherwise
-    """
-
-    # Get ethernet frame data
-    try:
-        ether_pkt = pkt[Ether]
-    except Exception as e:
-        if debug:
-            print(f'   # packet {pkt_label}')
-            print('      no ether header, could be STP')
-        return False
-
-    # Check for packets that are not IP
-    if 'type' not in ether_pkt.fields:
-        if debug:
-            print(f'    # packet {pkt_label}')
-            print('       lldp(probably) packet')
-        return False
-
-    # Check for packets that are ARP
-    if ether_pkt.type == 0x0806:
-        if debug:
-            print(f'    # packet {pkt_label}')
-            print('       arp packet')
-        return False
-
-    # Check ether type to ensure ipv4 or ipv6
-    if ether_pkt.type != 0x0800 and ether_pkt.type != 0x86DD:
-        if debug:
-            print(f'    # packet {pkt_label}')
-            print('       non-ip packet')
-        return False
-
-    # Check layer4 protocol type for support
-    if pkt[Ether][IP].proto in [1, 6, 17, 58]:
-        return True
-    else:
-        print(f'    # packet {pkt_label}')
-        print(f'      layer 4 protocol not recognized: {pkt[IP].proto}')
-        return False
-
 
 def get_l4_proto(pkt):
     match pkt[Ether][IP].proto:
@@ -84,40 +66,45 @@ def get_l4_proto(pkt):
             return False
 
 
-def pkt_list_to_dict(p_list):
+def pkt_list_to_dict(p_list,):
     """
     Take scapy packet list created from rdpcap, pull out relevant data and add to dict() for faster searching.
     In this case we add the packet data as the key, and the reference to the pkt_list index as value. It seems
     that it is much more efficient to search dict() by key rather than value.  And we want to keep the index
     so that we can later get full packet details from the packet list if we chose to do so.
-    :param p_list:
+    :param p_list:     list of packets to process and add data to dictionary for
     :return: dict()
     """
     p_dict = {}
-    for p_idx, p_pkt in enumerate(p_list):
-        if check_packet_attributes(p_pkt):
-            if get_l4_proto(p_pkt) == 'udp':
-                my_key = f'id={p_pkt[Ether][IP].id}, src={p_pkt[Ether][IP].src}, ' \
-                                 f'dst={p_pkt[Ether][IP].id}, sport={p_pkt[Ether][IP][UDP].sport}, ' \
-                                 f'dport={p_pkt[Ether][IP][UDP].dport}, proto=udp'
-                p_dict[my_key] = p_idx
+    for i, pkt in enumerate(p_list):
+        if get_l4_proto(pkt) == 'udp':
+            my_pkt = f'id={pkt[IP].id}, src={pkt[IP].src}, ' \
+                     f'dst={pkt[IP].dst}, sport={pkt[UDP].sport}, ' \
+                     f'dport={pkt[UDP].dport}, proto=udp'
 
-            if get_l4_proto(p_pkt) == 'tcp':
-                my_key = f'id={p_pkt[Ether][IP].id}, src={p_pkt[Ether][IP].src}, ' \
-                                f'dst={p_pkt[Ether][IP].id}, sport={p_pkt[Ether][IP][TCP].sport}, ' \
-                                f'dport={p_pkt[Ether][IP][TCP].dport}, proto=tcp'
-                p_dict[my_key] = p_idx
+            p_dict[my_pkt] = i
 
-            if get_l4_proto(p_pkt) == 'icmp':
-                my_key = f'id={p_pkt[Ether][IP].id}, src={p_pkt[Ether][IP].src}, ' \
-                                f'dst={p_pkt[Ether][IP].id}, sport={p_pkt[Ether][IP][ICMP].type}, ' \
-                                f'dport={p_pkt[Ether][IP][ICMP].code}, proto=icmp'
-                p_dict[my_key] = p_idx
+        if get_l4_proto(pkt) == 'tcp':
+            my_pkt = f'id={pkt[IP].id}, src={pkt[IP].src}, ' \
+                     f'dst={pkt[IP].dst}, sport={pkt[TCP].sport}, ' \
+                     f'dport={pkt[TCP].dport}, proto=tcp'
 
-            if get_l4_proto(p_pkt) == 'icmpv6':
-                my_key = f'id={p_pkt[Ether][IP].id}, src={p_pkt[Ether][IP].src}, ' \
-                                f'dst={p_pkt[Ether][IP].id}, sport={p_pkt[Ether][IP][ICMPv6].type}, ' \
-                                f'dport={p_pkt[Ether][IP][ICMPv6].code}, proto=icmpv6'
+            p_dict[my_pkt] = i
+
+        if get_l4_proto(pkt) == 'icmp':
+            my_pkt = f'id={pkt[IP].id}, src={pkt[IP].src}, ' \
+                     f'dst={pkt[IP].dst}, type={pkt[ICMP].type}, ' \
+                     f'code={pkt[ICMP].code}, proto=icmp'
+
+            p_dict[my_pkt] = i
+
+        if get_l4_proto(pkt) == 'icmpv6':
+            my_pkt = f'id={pkt[IP].id}, src={pkt[IP].src}, ' \
+                     f'dst={pkt[IP].dst}, type={pkt[ICMPv6].type}, ' \
+                     f'code={pkt[ICMPv6].code}, proto=icmpv6'
+
+            p_dict[my_pkt] = i
+
     return p_dict
 
 
@@ -127,26 +114,51 @@ def print_packet_details(pkt, pkt_label):
     :param pkt_label: label for packet when details output
     :return: return True if is known packet format and can be printed or return False
     """
-    print(f'    # packet {pkt_label} #')
-    print(f'      ip-id: {pkt[Ether][IP].id}')
-    print(f'      ip-src: {pkt[Ether][IP].src}')
-    print(f'      ip-dst: {pkt[Ether][IP].dst}')
+    if IP in pkt:
+        print(f'    # packet {pkt_label} #')
+        print(f'      ip-id: {pkt[IP].id}')
+        print(f'      ip-src: {pkt[IP].src}')
+        print(f'      ip-dst: {pkt[IP].dst}')
+    elif IPv6 in pkt:
+        print(f'    # packet {pkt_label} #')
+        print(f'      ip-id: {pkt[IPv6].id}')
+        print(f'      ip-src: {pkt[IPv6].src}')
+        print(f'      ip-dst: {pkt[IPv6].dst}')
+    else:
+        return False
 
-    match pkt[Ether][IP].proto:
+    match pkt[IP].proto:
         case 6:
             print(f'     l4-proto: tcp')
-            print(f'     tcp-sport: {pkt[Ether][IP][TCP].sport}')
-            print(f'     tcp-dport: {pkt[Ether][IP][TCP].dport}')
+            print(f'     tcp-sport: {pkt[TCP].sport}')
+            print(f'     tcp-dport: {pkt[TCP].dport}')
         case 17:
             print(f'     l4-proto: udp')
-            print(f'     udp-sport: {pkt[Ether][IP][UDP].sport}')
-            print(f'     udp-dport: {pkt[Ether][IP][UDP].dport}')
+            print(f'     udp-sport: {pkt[UDP].sport}')
+            print(f'     udp-dport: {pkt[UDP].dport}')
         case 1:
-            print(f'     icmp_type: {pkt[Ether][IP][ICMP].type}')
-            print(f'     icmp_code: {pkt[Ether][IP][ICMP].code}')
+            print(f'     icmp_type: {pkt[ICMP].type}')
+            print(f'     icmp_code: {pkt[ICMP].code}')
         case 58:
-            print(f'     icmp_type: {pkt[Ether][IP][ICMPv6].type}')
-            print(f'     icmp_code: {pkt[Ether][IP][ICMPv6].code}')
+            print(f'     icmp_type: {pkt[ICMPv6].type}')
+            print(f'     icmp_code: {pkt[ICMPv6].code}')
         case _:
             return False
     return True
+
+
+def check_pkt_support(pkt):
+    if IP in pkt:
+        # Check layer4 protocol type for support
+        if pkt[IP].proto in [1, 6, 17, 58]:
+            return True
+        else:
+            return False
+    elif IPv6 in pkt:
+        # Check layer4 protocol type for support
+        if pkt[IPv6].proto in [1, 6, 17, 58]:
+            return True
+        else:
+            return False
+    else:
+        return False
--- pcap-compare.py
+++ pcap-compare.py
@@ -72,6 +72,10 @@ list_pcap_files = args.list_pcaps
 flip_pcaps = args.flip_pcaps
 missing_pkts_to_pcap = args.output_pcap
 track_time = args.timer
+results_dir = args.results_dir
+max_pcap1_files = args.max_main_pcaps
+max_pcap2_files = args.max_match_pcaps
+max_packets_to_compare = args.max_main_packets
 
 # Directory Path, pcap files in this dir will be compared to pcaps in pcap2_dir to
 # check if packets from files in pcap1_dir exist in any of pcaps in  pcap2_dir
@@ -82,39 +86,29 @@ else:
     pcap1_dir = args.pcap_dir_main
     pcap2_dir = args.pcap_dir_match
 
-results_dir = args.results_dir
-max_pcap1_files = args.max_main_pcaps
-max_pcap2_files = args.max_match_pcaps
-max_packets_to_compare = args.max_main_packets
-
 # Program Initialization ##########
 start_time = time.time()
-missing_pkt_list = []
+missing_pkts = {}
+matched_pkt_keys = set()
 total_checked_pkts = 0
 total_matched_pkts = 0
+first_packet_time = None
+last_packet_time = None
 
 # Main() ##########################
-files_pcap1 = os.listdir(pcap1_dir)
-files_pcap2 = os.listdir(pcap2_dir)
+chk_pkt = check_pkt_support  # Assign chk_pkt_support() function to a variable
 
-# Check input files
-if len(files_pcap1) < 1:
+# process pcap dirs for pcap files
+files_pcap1 = get_pcap_files(pcap1_dir, max_pcap1_files)
+files_pcap2 = get_pcap_files(pcap2_dir, max_pcap2_files)
+
+if not files_pcap1:
     print('No pcap files found in pcap dir 1: Aborting')
     sys.exit(1)
 
-if len(files_pcap2) < 1:
+if not files_pcap2:
     print('No pcap files found in pcap dir 2: Aborting')
     sys.exit(1)
-
-for f in files_pcap1:
-    if pathlib.Path(f).suffix not in ['.pcap']:
-        print(f'Notice: file "{f}: in pcap_dir1 file extension not ".pcap" .out: SKIPPING')
-        files_pcap1.remove(f)
-
-for f in files_pcap2:
-    if pathlib.Path(f).suffix not in ['.pcap', '.out']:
-        print(f'Notice: file "{f}" in pcap_dir2 file extension not ".pcap": SKIPPING')
-        files_pcap2.remove(f)
 
 print(f'INFO: # files in "main" pcap dir (pcap1): {len(files_pcap1)}')
 print(f'INFO: # files in "match" pcap dir (pcap2): {len(files_pcap2)}')
@@ -133,36 +127,66 @@ if list_pcap_files:
 
 # Start processing of each file in pcap_dir1 ########################################
 for p1_count, f1 in enumerate(files_pcap1):
-
-    print(f'###### ({p1_count +1} of ({len(files_pcap1)}) Reading {f1} to memory. ', end='')
+    print(f'###P1# ({p1_count +1} of {len(files_pcap1)}) Reading {f1} to memory. ', end='')
     # Use scapy rdpcap() to read pcap (or pcapng) to memory as scapy.plist.PacketList
     try:
         pkt_list1 = rdpcap(f'{pcap1_dir}/{f1}')
     except Exception as e:
         print(f'Error: Unable to read file {f1} as pcap: Aborting')
+        print(e)
         sys.exit(1)
 
+    # Pass the check_pkt_support function to the scapy PacketList filter function to
+    # filter for supported packet types then re-assign pkt_list1 with the results
     print(f'Number of packets: {len(pkt_list1)}')
+    pkt_list1 = pkt_list1.filter(chk_pkt)
+    total_checked_pkts += len(pkt_list1)
+
+    # Save some packet timestamps for later analysis
+    if p1_count == 0:
+        first_packet_time = pkt_list1[0].time
+
+    if p1_count == len(files_pcap1):
+        last_packet_time = pkt_list1[p1_count].time
+
+    # If tracking missing packets, we make a copy of all packets to new dict()
+    # will then subtract matched packets from this dict().
+    # This is not efficient for memory or cpu, but we need to create a key
+    # from a list with no keys to track the packet and remove later if needed
+    if missing_pkts_to_pcap and do_compare:
+        for i, p in enumerate(pkt_list1):
+            # have to give a unique key for each packet from each main (p1) file
+            missing_pkts[f'{p1_count}:{i}'] = p
 
     # To optimize for speed, adding relevant packet data to a dict as a string in as key in dict
     # with the original packet list index as the value. (easier/faster to compare dictionary keys vs values)
     p1_dict = pkt_list_to_dict(pkt_list1)
-    total_checked_pkts += len(p1_dict)
 
-    # Start process of each file in pcap_dir2 ######################################
+    # Start process of each file in pcap_dir2 ########################################
     # Compare pcap from dir1 to every pcap from dir2 (up to max defined # of files to compare)
     for p2_count, f2 in enumerate(files_pcap2):
-        matched_pkt_keys = set()
-        missing_pkt_keys = set()
-
-        print(f'  #### ({p2_count +1} of ({len(files_pcap2)}) Reading {f2} to memory. ', end='')
+        matched_pkts_per_p2 = 0
+        print(f'  ##P2# ({p2_count +1} of {len(files_pcap2)}) Reading {f2} to memory. ', end='')
         # Use scapy redcap() to read pcap (or pcapng) to memory as scapy.plist.PacketList
-        pkt_list2 = rdpcap(f'{pcap2_dir}/{f2}')
+        try:
+            pkt_list2 = rdpcap(f'{pcap2_dir}/{f2}')
+        except Exception as e:
+            print(f'Error: Unable to read file {f2} as pcap: Aborting')
+            print(e)
+            sys.exit(1)
+
         print(f'Number of packets: {len(pkt_list2)}')
 
-        # To optimize for speed, adding relevant packet data to a set as a string
+        # pass the check_pkt_support function to the scapy PacketList filter function to
+        # filter for supported packet types then re-assign pkt_list1 with the re
+        pkt_list2 = pkt_list2.filter(chk_pkt)
+
+        # To optimize for speed, adding relevant packet data to a dict as a string
         # We should then be able to compare sets of packets from both pcaps much faster
         p2_dict = pkt_list_to_dict(pkt_list2)
+
+        # Recover some memory
+        pkt_list2 = ''
 
         # Start to check if packets in pcap file from dir1 are in any pcap files in dir2
         if do_compare:
@@ -180,12 +204,15 @@ for p1_count, f1 in enumerate(files_pcap1):
             # match a matched packet again.
             for k in tmp_p1_dict:
                 if k in p2_dict:
-                    matched_pkt_keys.add(p1_dict[k])
+                    # keep track of all packets matched
+                    matched_pkt_keys.add(f'{p1_count}:{tmp_p1_dict[k]}')
+                    total_matched_pkts += 1
+                    matched_pkts_per_p2 += 1
+                    # Delete matched packets from original packet list to avoid further checking for this pkt
                     del p1_dict[k]
 
             print(f'      Valid Packets to Check: {len(tmp_p1_dict)}')
-            print(f'      Number of matched packets this run: {len(matched_pkt_keys)}')
-            total_matched_pkts += len(matched_pkt_keys)
+            print(f'      Number of matched packets this run: {matched_pkts_per_p2}')
             print(f'      Total matched packets for all runs: {total_matched_pkts}')
 
             # check modifications that have been made to the "main" p1_dict (verifying packets are removed if matched)
@@ -196,29 +223,9 @@ for p1_count, f1 in enumerate(files_pcap1):
         # for current main file and get next pcap1
         if len(p1_dict) == 0:
             if debug:
-                print('####### No available packets in current p1 packet list, move to next p1')
+                print('####### No packets left in current p1 packet list, move to next p1')
             break
 
-        if p2_count + 1 >= max_pcap2_files > 0:
-            if missing_pkts_to_pcap and do_compare and len(p1_dict) > 0:
-                # Update list of packets not matched in current p1 file to any p2 files and save the original packets
-                missing_pkt_list.extend(pkt_list1)
-
-            if debug:
-                print(f'####### Max pcap2 files to process reached: {max_pcap2_files}')
-            break
-
-    if p1_count + 1 >= max_pcap1_files > 0:
-        if missing_pkts_to_pcap and do_compare and len(p1_dict) > 0:
-            # Update list of packets not matched in current p1 file to any p2 files and save the original packets
-            missing_pkt_list.extend(pkt_list1)
-        if debug:
-            print(f'######## Max pcap1 files to process reached: {max_pcap1_files}')
-        break
-
-    if missing_pkts_to_pcap and do_compare and len(p1_dict) > 0:
-        # Update list of packets not matched in current p1 file to any p2 files and save the original packets
-        missing_pkt_list.extend(pkt_list1)
 
 # Print total number of matched and missing packets from total of all files in pcap_dir1 (or pcap_dir2 if flipped)
 print('#############################################')
@@ -232,21 +239,35 @@ print('#############################################')
 if track_time:
     print(f'Run time: {str(datetime.timedelta(seconds=int(time.time()) - int(start_time)))}')
 
+# Write missing packets to pcap (if enabled)
 if missing_pkts_to_pcap and do_compare:
     # If enabled, write missing packets to new pcap file
-    if not len(missing_pkt_list) > 0:
-        print('No missing packets to write to pcap: skipping')
-    else:
-        now = datetime.datetime.now()
+    now = datetime.datetime.now()
+    out_pkts = []
 
-        print(f'Start writing missing Packets PCAP: '
+    # Remove all matched packets from the missing pkt list dict()
+    for i in matched_pkt_keys:
+        del missing_pkts[i]
+
+    if len(missing_pkts) > 0:
+        # Copy all pkts (val) in missing_pkts_list to out_pkts list (for write to pcap)
+        # basically converting missing pkts from dict() to list()
+        for i in missing_pkts:
+            out_pkts.append(missing_pkts[i])
+
+        print(f'Start writing missing Packets ({len(missing_pkts)}) to PCAP: '
               f'{results_dir}/missing_pkts-{now.strftime("%Y-%m-%d_%H-%M")}.pcap')
+
         try:
-            wrpcap(f'{results_dir}/missing_pkts-{now.strftime("%Y-%m-%d_%H-%M")}.pcap', missing_pkt_list)
+            wrpcap(f'{results_dir}/missing_pkts-{now.strftime("%Y-%m-%d_%H-%M")}.pcap', out_pkts)
         except Exception as e:
             print(e)
             print(f'Error: Unable to write to pcap file in dir: {results_dir}')
         else:
             print(f'Success writing pcap: '
                   f'{results_dir}/missing_pkts-{now.strftime("%Y-%m-%d_%H-%M")}.pcap')
-                                                                                                                                                                                                                                                                                                                                                                                                              # wrpcap(f'{results_dir}/missing_packets.pcap', missing_packets)
+    else:
+        print('No missing packets to write to pcap: Skipping')
+
+print(f'first packet timestamp: {first_packet_time}')
+print(f'last packet timestamp: {last_packet_time}')
